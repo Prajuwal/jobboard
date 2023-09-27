1182cdd10250b8cce4d701ab30a10dbb1bc3867e
Prajuwal <prajuwalsingh@gmail.com>
2023-09-28 02:25:30 +0530

    UI for first 9 postings ready

--- src/App.jsx
+++ src/App.jsx
@@ -6,20 +6,25 @@ function App() {
   return (
     <>
       <div className="flex flex-col items-center">
-        <h1 className="text-center mt-10 text-3xl">Hacker News Job</h1>
+        <h1 className="text-center mt-5 text-3xl">Hacker News Job</h1>
         <div className="">
-          <div className="flex justify-center space-x-4 mt-20">
+          <div className="flex justify-center space-x-4 mt-10 w-96 h-40">
             <Card />
             <Card />
             <Card />
           </div>
-          <div className="flex justify-center space-x-4 mt-10">
+          <div className="flex justify-center space-x-4 mt-10 w-96 h-40">
+            <Card />
+            <Card />
+            <Card />
+          </div>
+          <div className="flex justify-center space-x-4 mt-10 w-96 h-40">
             <Card />
             <Card />
             <Card />
           </div>
         </div>
-        <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full mt-4">
+        <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full mt-8">
           Load More
         </button>
       </div>
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,24 +1,23 @@
-import React from 'react'
+import React from "react";
 
 const Card = () => {
   return (
     <div class="max-w-xs p-6 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
       <a href="#">
-        <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
-          Noteworthy technology acquisitions 2021
+        <h5 class="mb-2 text-xl font-bold tracking-tight text-gray-900 dark:text-white">
+          Noteworthy technology
         </h5>
       </a>
       <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">
-        Here are the biggest enterprise technology acquisitions of 2021 so far,
-        in reverse chronological order.
+        Here are 
       </p>
       <a
         href="#"
-        class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+        class="inline-flex items-center px-3 py-1 text-m font-small text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
       >
         Read more
         <svg
-          class="w-3.5 h-3.5 ml-2"
+          class="w-2 h-2 ml-2"
           aria-hidden="true"
           xmlns="http://www.w3.org/2000/svg"
           fill="none"
@@ -35,6 +34,6 @@ const Card = () => {
       </a>
     </div>
   );
-}
+};
 
-export default Card
+export default Card;
